abfec8476791542b69d4840b5b4200eabf9af08d
Max Robles <maxirobles1234@gmail.com>
2023-10-25 16:09:29 +0200

    button to add one to the value

--- app.js
+++ app.js
@@ -10,4 +10,21 @@ button.addEventListener('click', changeB)
 function changeB() {
     const colorIndex = parseInt(Math.random()*color.length)
     body.style.background = color [colorIndex] 
+
+
 }
+
+function clicked() {
+
+    let interno = document.querySelector("#Numero")
+    interno.innerHTML += +interno.innerHTML +1
+
+}
+
+// Agrega un boton que reinicie el color de fondo y reinicie el valor a 0
+function Reset() {
+    
+}
+
+
+// Agrega un boton que cambie los valores internos entre los 2 botones (click here <-> numero)
