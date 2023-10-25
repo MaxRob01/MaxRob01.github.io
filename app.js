abd08972880131de4ae3e0d982815883f872caed
Max Robles <maxirobles1234@gmail.com>
2023-10-25 18:00:32 +0200

    creat the reset button

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
-const button = document.querySelector("button")
-const body = document.querySelector("body")
-const color = ['red', 'green', 'pink', 'purple']
+const button = document.querySelector("#changeB");
+const body = document.querySelector("body");
+const color = ["red", "green", "pink", "purple"];
 
-body.style.backgroundColor = "violet"
+body.style.backgroundColor = "violet";
 
-
-button.addEventListener('click', changeB)
-
+//change background color
 function changeB() {
-    const colorIndex = parseInt(Math.random()*color.length)
-    body.style.background = color [colorIndex] 
-
-
+	const colorIndex = parseInt(Math.random() * color.length);
+	body.style.background = color[colorIndex];
 }
-
-function clicked() {
-
-    let interno = document.querySelector("#Numero")
-    interno.innerHTML += +interno.innerHTML +1
-
+// add one number to the button
+function addOne() {
+	let interno = document.querySelector("#Numero");
+	interno.innerHTML = Number(interno.innerHTML) + 1;
 }
 
 // Agrega un boton que reinicie el color de fondo y reinicie el valor a 0
+
+// reset the two last buttons to initial values
 function Reset() {
-    
+    // Reset number
+    let interno = document.querySelector("#Numero");
+	interno.innerHTML = 0;
+
+    // Reset background
+    body.style.backgroundColor = "violet";
+
 }
 
 
