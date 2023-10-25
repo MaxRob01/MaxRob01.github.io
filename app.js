2f99c3400e06331f2299d8a44559f17d330369be
Max Robles <maxirobles1234@gmail.com>
2023-10-25 14:40:39 +0200

    button to change background color

---
+++ app.js
@@ -0,0 +1,13 @@
+const button = document.querySelector("button")
+const body = document.querySelector("body")
+const color = ['red', 'green', 'pink', 'purple']
+
+body.style.backgroundColor = "violet"
+
+
+button.addEventListener('click', changeB)
+
+function changeB() {
+    const colorIndex = parseInt(Math.random()*color.length)
+    body.style.background = color [colorIndex] 
+}
