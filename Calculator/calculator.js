b348bf9a40628116750e16b6fc3c67bc8734662e
Max Robles <maxirobles1234@gmail.com>
2023-10-26 14:14:13 +0200

    I created one function for each arithmetic operation. I also declared the variables and got the values from the number inputs. I convert them to numbers.

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,2 +1,32 @@
-console.log('works')
+console.log("works");
+
+function sumar() {
+	let firstInput = parseInt(document.getElementById("first").value);
+	let secondInput = parseInt(document.getElementById("second").value);
+	let sumInputs = firstInput + secondInput;
+	console.log(sumInputs);
+}
+
+function restar() {
+	let firstInput = parseInt(document.getElementById("first").value);
+	let secondInput = parseInt(document.getElementById("second").value);
+	let subInputs = firstInput - secondInput;
+	console.log(subInputs);
+}
+
+function multiplicar() {
+	let firstInput = parseInt(document.getElementById("first").value);
+	let secondInput = parseInt(document.getElementById("second").value);
+	let multInputs = firstInput * secondInput;
+	console.log(multInputs);
+}
+
+function dividir() {
+	let firstInput = parseInt(document.getElementById("first").value);
+	let secondInput = parseInt(document.getElementById("second").value);
+	let divInputs = firstInput / secondInput;
+	console.log(divInputs);
+}
+
+
 
