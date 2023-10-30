5d8b7523bf53d79606de53322b59f50097dd64b9
Max Robles <maxirobles1234@gmail.com>
2023-10-30 12:08:53 +0100

    Captured the data from the inputs

--- Exp-Tracker.js
+++ Exp-Tracker.js
@@ -1,1 +1,14 @@
-console.log('linked')
+console.log("linked");
+//declarar una funcion para el boton addExpense
+function addExpense() {
+	//declarar una variable y asignarle el valor del input 'description'
+	let expense = {
+		description: document.querySelector("#description").value,
+		amount: document.querySelector("#amount").value,
+		category: document.getElementById('categoryList').value,
+	};
+	console.log(expense.description);
+	//declarar una variable y asignarle el valor del input 'amount'
+	//declarar una variable y asignarle el valor del input 'category'
+	//declarar una variable y asignarle un list element tag (li)
+}
