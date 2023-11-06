5ceab49038cfe03586d6045b56e08df5b75a89a3
Max Robles <maxirobles1234@gmail.com>
2023-11-06 13:30:11 +0100

    Added the function to select a category

--- Exp-Tracker.js
+++ Exp-Tracker.js
@@ -10,10 +10,14 @@ function addExpense() {
 	//declarar una variable y asignarle un list element tag (li)
 
 	let li = document.createElement("li");
+	//console.log(li)
+	//console.log(typeof li);
 	li.textContent = `${expense.description}: $${expense.amount} - ${expense.category}`;
 
 	//step 0: add a class to each li element that you create programmatically.
+	//console.log(typeof li.className)
 	li.className = expense.category;
+	//console.log( li.className);
 
 	//agregar una funcion que borre el contenido de los inputs
 	document.querySelector("#description").value = "";
@@ -28,15 +32,24 @@ function addExpense() {
 
 	totalExpenses.innerHTML = Number(totalExpenses.innerHTML) + +expense.amount;
 
-	let filterSelect = document.getElementById("filterSelect");//declaro una variable para guardar el elemento del HTML
-	filterSelect.addEventListener("change", filterExpenses);//agrego el event listener a la variable y asigno la funcion
+	let filterSelect = document.getElementById("filterSelect"); //declaro una variable para guardar el elemento del HTML
+	filterSelect.addEventListener("change", filterExpenses); //agrego el event listener a la variable y asigno la funcion
 	//console.log(filterSelect)
-	function filterExpenses() { //esta es la funcion que corre el event listener
-		let selectCategory = filterSelect.value;//declaro una variable y le asigno el valor seleccionado en el dropdown
-		let expensesItems = document.querySelectorAll('#expensesList li')//selecciona todas las listas dentro de la ul
-        console.log(expensesItems);
+	function filterExpenses() {
+		//esta es la funcion que corre el event listener
+		let selectCategory = filterSelect.value; //declaro una variable y le asigno el valor seleccionado en el dropdown
+		console.log(selectCategory);
+		let expensesItems = document.querySelectorAll("#expensesList li"); //selecciona todas las listas dentro de la ul
+		console.log(expensesItems);
 
-        
+		expensesItems.forEach((item) => {
+			if (item.className == selectCategory || selectCategory == 'All'  ) {
+				item.style.display = "";
+
+			} else {
+				item.style.display = "none";
+			}
+		});
 	}
 
 	let removalButton = document.createElement("button");
