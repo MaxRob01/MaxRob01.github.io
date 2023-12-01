2f87b544f53e65db54491c19314c9d962b6911c5
Max Robles <maxirobles1234@gmail.com>
2023-12-01 23:45:16 +0100

    Add functions to get the input text and API info

--- weather-app/weather.js
+++ weather-app/weather.js
@@ -1,16 +1,27 @@
-console.log('works')
+console.log("works");
 
-var city = document.getElementById(yourCity)
-
-
-async function getWeatherInfo (city) {
-    try {
-        let theWeather = await fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b2a35c7e3ce0d3c6cf840c45396ced43')
-    } catch (err) {
-    console.error('did not work')
- }
+function getCity() {
+	var city = document.getElementById("yourCity").value;
+	console.log(city);
 }
 
+async function getWeatherInfo(city) {
+	try {
+		let response = await fetch(
+			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b2a35c7e3ce0d3c6cf840c45396ced43`
+		);
 
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
 
+		let data = await response.json();
+		console.log("###############the result is:");
+		console.log(data);
+		return data;
+	} catch (error) {
+		console.error("Error fetching weather data: ", error);
+	}
+}
 
+getWeatherInfo(city);
