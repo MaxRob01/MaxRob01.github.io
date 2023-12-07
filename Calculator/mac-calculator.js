159736e964fc971fd78819992366c5f7ab914ba7
Max Robles <maxirobles1234@gmail.com>
2023-12-07 23:58:37 +0100

    Add logic to do percentage

--- Calculator/mac-calculator.js
+++ Calculator/mac-calculator.js
@@ -14,14 +14,12 @@ function getButtonValue(value) {
 		valor2 = value;
 		isSecondValue = false;
 	}
-
-	//console.log(valor1, valor2);
 	return value;
 }
 
-/*function clearInput () {
+function clearInput() {
 	document.getElementById("display").value = "";
-}*/
+}
 
 function getOperator(value) {
 	document.getElementById("display").value = value;
@@ -31,20 +29,17 @@ function getOperator(value) {
 function operation() {
 	if (operator == "+") {
 		result = Number(valor1) + Number(valor2);
-		//console.log(result);
 	} else if (operator == "-") {
 		result = Number(valor1) - Number(valor2);
-		//console.log(result);
 	} else if (operator == "*") {
 		result = Number(valor1) * Number(valor2);
-		//console.log(result);
 	} else if (operator == "÷") {
 		result = Number(valor1) / Number(valor2);
-		//console.log(result);
+	} else if (operator == "%") {
+		result = (Number(valor1) * Number(valor2)) / 100;
 	}
-	return result
+	document.getElementById("display").value = result;
+	return result;
 }
 
-//cuando hago click en el boton de la operacion guardar el valor del input en una variable y guardar la operacion en una segunda variable
-//crar una funcion "operacion"
-//cuando hago click en boton igual asigne el valor del input y corra una funcion que haga la operacion
+
