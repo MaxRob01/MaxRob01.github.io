785aae61fbe71be82b01567299cb7d7e20051170
Max Robles <maxirobles1234@gmail.com>
2023-12-08 17:52:57 +0100

    Add logic to reset the calculator

--- Calculator/mac-calculator.js
+++ Calculator/mac-calculator.js
@@ -12,17 +12,32 @@ function getButtonValue(value) {
 		valor2 ? (valor2 = valor2 + value) : (valor2 = value);
 		document.getElementById("display").value = valor2;
 	}
+	console.log(valor1 + " getvalue v1");
+	console.log(valor2 + " getvalue v2");
+	console.log(result + " getvalue result");
+	console.log(isSecondValue + " getButtonValue");
 	return value;
 }
 
 function clearInput() {
-	valor1 = document.getElementById("display").value = "";
+	valor1 = "";
+	valor2 = "";
+	result = "";
+	isSecondValue = false;
+	document.getElementById("display").value = "";
+
+	console.log(valor1 + " clearinput v1");
+	console.log(valor2 + " clearinput v2");
+	console.log(result + " clearinput result");
+	
+	console.log(isSecondValue + " clearInput")
 }
 
 function getOperator(value) {
 	document.getElementById("display").value = value;
 	operator = value;
 	isSecondValue = true;
+	console.log(isSecondValue + " getOperator");
 }
 
 function operation() {
@@ -38,6 +53,7 @@ function operation() {
 		result = (Number(valor2) * Number(valor1)) / 100;
 	}
 	document.getElementById("display").value = result;
+	console.log(isSecondValue + " operation");
 	return result;
 }
 
@@ -47,3 +63,4 @@ function switchSign() {
 	var inputNumb = (document.getElementById("display").value = numberSign);
 }
 
+/// A resolover: AC button tiene que resetear todo. var result tiene que poder recibir una nueva operacion.
