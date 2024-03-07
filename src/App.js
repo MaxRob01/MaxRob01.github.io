e219ec170ae0a3e45cfa2be6991aff613917b55b
Max Robles <maxirobles1234@gmail.com>
2024-03-07 12:37:32 +0100

    add basic structure components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import TextContainer from "./Text-container";
+import Sidebar from "./Sidebar";
+import Footer from "./Footer";
+import Header from "./Header";
+import Body from "./Body";
+import MainContainer from "./MainContainer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<MainContainer>
+			<Header className="App-header">Typing Speed Test</Header>
+			<Body className="App-body">
+				<Sidebar></Sidebar>
+				<TextContainer>
+					<h1>"Hello"</h1>
+				</TextContainer>
+				<Sidebar></Sidebar>
+			</Body>
+			<Footer></Footer>
+		</MainContainer>
+	);
 }
 
 export default App;
